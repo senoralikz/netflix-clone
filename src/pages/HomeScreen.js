ec9f6a5ae2b438cc7a89917fc6233b5879820f76
senoralikz <alexjm825@gmail.com>
2022-10-09 15:33:13 -0400

    added homescreen page and navbar component

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Nav from "../components/Nav";
+import "../styles/HomeScreen.css";
+
+function HomeScreen() {
+  return (
+    <div className="homescreen">
+      {/* Nav */}
+      <Nav />
+      {/* Banner */}
+      {/* Row */}
+    </div>
+  );
+}
+
+export default HomeScreen;
