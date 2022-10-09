4847c3b597ccd0c0eef5dc3589e6fecf28c4e64e
senoralikz <alexjm825@gmail.com>
2022-10-09 13:41:46 -0400

    added firebase config and removed info from README and unnecessary files

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD1BknehyehtZ5LXNixZbDOvhR2_tjmoTI",
+  authDomain: "netflix-clone-3511d.firebaseapp.com",
+  projectId: "netflix-clone-3511d",
+  storageBucket: "netflix-clone-3511d.appspot.com",
+  messagingSenderId: "455999533747",
+  appId: "1:455999533747:web:e8861988a1cd33efacf6c6",
+};
